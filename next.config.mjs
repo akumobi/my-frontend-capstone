b2b324810f891643ae2767d3bde4566ee72e0abd
Akumobi Kingsley <akumobi@gmail.com>
2024-12-11 20:35:01 +0100

    feat: update config files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
-export default nextConfig;
+export const images = {
+    remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'cdn.prod.website-files.com',
+            port: '',
+            pathname: '/**',
+        },
+    ],
+};
+// export default nextConfig;
