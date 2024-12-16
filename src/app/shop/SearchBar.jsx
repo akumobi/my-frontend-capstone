b0244d494e6ab87de1f21c47477dedac815f0fa5
Akumobi Kingsley <akumobi@gmail.com>
2024-12-17 00:49:23 +0100

    fix: debug style error in search component

--- src/app/shop/SearchBar.jsx
+++ src/app/shop/SearchBar.jsx
@@ -5,7 +5,7 @@ export default function SearchBar({holder, title, handle}) {
   return (
     <div>
         <input type="text" name="search" required className={`${style2.searchBar} ${style2.wInput}`} placeholder={holder} defaultValue={title} onChange={handle}/>
-        <input type="submit" name="search" value="search" className={style2.searchButton} {...style2.wButton}/>
+        <input type="submit" name="search" value="search" className={`${style2.searchButton} ${style2.wButton}`}/>
     </div>
   )
 }
